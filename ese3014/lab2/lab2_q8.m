b106402dfcaab973b74fbfc8c2d8dec22770e92b
Ronakkumar Sharma <51168291+sharmaronak79@users.noreply.github.com>
2020-01-27 13:18:19 -0500

    Add files via upload

---
+++ ese3014/lab2/lab2_q8.m
@@ -0,0 +1,10 @@
+% Q:-8 Create a function that calculates the area of a circle with the radius as input in command window. Call this function from the MATLAB command
+%      line or in a MATLAB script (.m).
+clc;close all;
+r=input("Enter the radius of Circle : ")
+
+disp(" Area of circle is : ")
+A= pi*r*r
+
+
+
